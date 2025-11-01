2c54a60081b3e543726777bee3dc5b2a7a5deff3
Ahmvdaziz <145570464+Ahmvdaziz@users.noreply.github.com>
2025-11-01 18:01:43 +0200

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,54 @@
+#include <iostream>
+
+using namespace std;
+
+int main()
+{
+/*
+    char character;
+    int num=0;
+    cout<<"plz enter the character you want to convert to ascii"<<endl;
+    cin>>character;
+    cout<<"The ascii value of "<<character<<" is "<< int(character)<<endl;
+    cout<<"\nPlease enter the number you want to convert to a character " << endl;
+    cin >> num;
+    cout<<"The character for ascii value "<<num<<"is "<<char(num)<<endl;
+*/
+
+/*
+    double num1 = 0, num2 = 0;
+    cout<<"Plzz enter the 2 numbers you want to perform operations on : "<<endl;
+    cin>>num1>>num2;
+    char opearion;
+    cout<<"Enter opeartion do you want to do"<<endl;
+    cin>>opearion;
+    if(opearion=='+'){
+        cout<<"Addition is "<<num1+num2<<endl;
+    }
+    else if(opearion=='-'){
+        cout<<"Subtraction is "<<num1-num2<<endl;
+    }
+    else if(opearion=='/'){
+        cout<<"Division is  "<<num1/num2<<endl;
+    }
+    else{
+            cout<<"Multiplication is "<<num1*num2<<endl;
+    }
+/*
+
+    int choies;
+    cout<<"Plzzz enter the char do you want to convert to captel or small if captel write 1 else 2"<<endl;
+    cin>>choies;
+    cout<<"enter char ";
+    cin>> character;
+    if(choies==1){
+        cout<<"The Lowercase character is " << char(character+32)<<endl;
+    }
+    else{
+        cout<<"The uppercase character is " << char (character-32)<<endl;
+    }
+
+*/
+   // cout<<"HI"<<endl;
+    return 0;
+}
